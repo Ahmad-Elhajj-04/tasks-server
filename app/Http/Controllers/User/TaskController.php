045b6375731a9b5a7a016920e758311b8f9e7f70
Charbel <charbel.dawoud@gmail.com>
2025-11-26 19:04:33 +0100

    first lesson

--- app/Http/Controllers/User/TaskController.php
+++ app/Http/Controllers/User/TaskController.php
@@ -5,17 +5,23 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Task;
+use App\Services\TaskServices;
 
 class TaskController extends Controller{
     
     function getAllTasks($id = null){
         if(!$id){
-            $tasks = Task::all();
-            return response()->json(["data" => $tasks]);
+            return Task::all();
         }
 
         $task = Task::find($id);
         return $task;
+
+        //$tasks = TaskService::getAllTasks();
+        //return response()->json(["data" => $tasks]);
+        //SERVICES NEVER RETURN JSON
+        //TRY AND CATCH ARE IN CONTROLLERS
+
     }
 
 }
---
+++ app/Services/TaskServices.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services;
+
+class TaskServices
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
