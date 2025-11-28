7bb0e362f23545e7b986c4fc2f96dc82cb3459cb
Charbel <charbel.dawoud@gmail.com>
2025-11-28 19:01:28 +0100

    jwt

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\User\TaskController;
 use App\Http\Controllers\Admin\TaskController as TaskAdminController;
 
-Route::group(["prefix" => "v0.1"], function(){
-    //Authenticated Routes
+//Authenticated Routes
+Route::group(["prefix" => "v0.1", "middleware" => "auth:api"], function(){
     Route::group(["prefix" => "user"], function(){
         Route::get('/tasks/{id?}', [TaskController::class, "getAllTasks"])->name("task-listing");
         Route::post('/add_update_task/{id?}', [TaskController::class, "addOrUpdateTask"]);
@@ -20,9 +21,9 @@ Route::group(["prefix" => "v0.1"], function(){
 });
 
 //Unauthenticated Routes
-Route::post('/login', [TaskController::class, "addTask"]);
-Route::post('/register', [TaskController::class, "addTask"]);
-
+Route::post('/login', [AuthController::class, "login"]);
+Route::post('/register', [AuthController::class, "register"]);
+Route::get('/error', [AuthController::class, "displayError"])->name("login");
 
 
 
